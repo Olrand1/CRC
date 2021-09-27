5e7ffdd9b90df9ff0a41ebec3f935971a144e9ab
AdeleNikitina <57428032+AdeleNikitina@users.noreply.github.com>
2021-09-27 11:24:40 +0300

    Тесты на создание расходного договора

--- src/test/java/tests/DocumentsTests/ExpenditureContractCreateTests.java
+++ src/test/java/tests/DocumentsTests/ExpenditureContractCreateTests.java
@@ -1,22 +1,28 @@
 package tests.DocumentsTests;
 
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
 import org.testng.annotations.Test;
 import tests.TestBase;
 
 public class ExpenditureContractCreateTests extends TestBase {
 
+  @Epic(value = "Расходные документы")
+  @Feature(value = "Создание расходного договора. Предмет договора не требует указания адреса")
   @Test
   public void createContractWithoutAddress() throws InterruptedException {
     app.loginPage().login("nikitina-ar", "qwerty");
-    Thread.sleep(30000);
+    Thread.sleep(6000);
     app.createDocumentPage().openCreateContractPage();
     app.createDocumentPage().selectContractFile();
     app.createDocumentPage().goToNextStep();
+    Thread.sleep(900);
     app.createDocumentPage().fillCommonExpenditureContractData("Региональный центр развития информационных систем (г. Пермь) РИЗП-0333", "Канцелярские товары");
     app.createDocumentPage().goToNextStep();
+    Thread.sleep(900);
     app.createDocumentPage().selectContractor("Anderson Group");
     app.createDocumentPage().goToNextStep();
-    Thread.sleep(30000);
+    Thread.sleep(900);
     app.createDocumentPage().fillMaxSum("60000");
     app.createDocumentPage().goToNextStep();
     app.createDocumentPage().sentToAgreement();
